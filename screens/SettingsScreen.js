339212927d6127b9b8f1f4445db61ab9fe1eeefb
Wayne Li <wli2@berkeley.edu>
2019-09-01 21:10:43 -0700

    Add basic methods that show most recent 10 notifications

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,81 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+import * as firebase from 'firebase';
+
+export default class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notifications: [],
+    };
+  }
+
+  async componentDidMount() {
+    var notifications = [];
+    await firebase
+      .database()
+      .ref('notification')
+      .orderByChild("timestamp")
+      .limitToLast(10)
+      .once('value')
+      .then(function(data) {
+        data.forEach(function(childNode) {
+          var notification = childNode.val();
+          notifications.unshift(notification);
+        });
+        this.setState({
+          notifications: notifications
+        });
+      }.bind(this));
+  }
+
+  renderNotifications() {
+    return this.state.notifications.map((notification, index) =>
+      <Text key={index}>{index}. Title: {notification.text}, Text: {notification.title}, Timestamp: {notification.timestamp}</Text>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            {this.renderNotifications()}
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
-SettingsScreen.navigationOptions = {
-  title: 'app.json',
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  view: {
+    position: 'absolute',
+    backgroundColor: 'transparent'
+  },
+});
