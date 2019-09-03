3ae904e1fe0c1625d61ff0830b326002394b9ff4
Wayne Li <wli2@berkeley.edu>
2019-09-02 18:09:08 -0700

    Add hours to mobile app

--- screens/HoursScreen.js
+++ screens/HoursScreen.js
@@ -6,6 +6,10 @@ import {
   View,
 } from 'react-native';
 
+import * as firebase from 'firebase';
+
+import Days from '../constants/Days';
+
 export default class HoursScreen extends React.Component {
   static navigationOptions = {
     header: null
@@ -13,9 +17,49 @@ export default class HoursScreen extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      daysToHours: {},
+    };
   }
 
   async componentDidMount() {
+    var daysToHours = {};
+    await firebase
+      .database()
+      .ref('info')
+      .once('value')
+      .then(function(data) {
+        const DB_DAYS_KEYS_TO_READABLE_DAYS = Days.DB_DAYS_KEYS_TO_READABLE_DAYS;
+        data.forEach(function(childNode) {
+          var dayKey = childNode.key;
+          if (DB_DAYS_KEYS_TO_READABLE_DAYS.hasOwnProperty(dayKey)) {
+            var day = childNode.val();
+            var readableDay = DB_DAYS_KEYS_TO_READABLE_DAYS[dayKey];
+            daysToHours[readableDay] = {
+              day: readableDay,
+              hours: day['12hours'],
+            };
+          }
+        });
+        this.setState({
+          daysToHours: daysToHours
+        });
+      }.bind(this));
+  }
+
+  renderHours() {
+    var orderedDaysToHours = Days.ORDERED_READABLE_DAYS.map((readableDay) => {
+      return this.state.daysToHours[readableDay];
+    });
+
+    console.log(orderedDaysToHours);
+
+    return orderedDaysToHours.map((dayObject) => {
+      if (dayObject) {
+        return <Text key={dayObject.day}>{dayObject.day} | Hours: {dayObject.hours}</Text>;
+      }
+    });
   }
 
   render() {
@@ -25,7 +69,7 @@ export default class HoursScreen extends React.Component {
           style={styles.container}
           contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
-            <Text>Hello World!</Text>
+            {this.renderHours()}
           </View>
         </ScrollView>
       </View>
